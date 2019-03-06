e8fcdb986ab9faab2e4e11ae7fb01ee6b7664f30
Riccardo <riccardo_london@live.it>
2019-03-06 13:24:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,14 @@
 def loop
-
   def countdown (num)
-
-    while num is > 0 do
+    if num == 12 
+      
+      return "HAPPY NEW YEAR!"
+    else while num is > 0 && < 12 do
     puts "#{num} SECOND(S)!"
     num -= 1
     end
   end
-
-return "HAPPY NEW YEAR!"
+  
 end
 
 def countdown_with_sleep(num)
